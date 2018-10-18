1fed1ac55a5514f40b426572fc9cebfd2c5bf9a2
thiam97 <34570173+thiam97@users.noreply.github.com>
2018-10-18 08:26:05 +0200

    Add files via upload

---
+++ data/create.sql
@@ -0,0 +1,16 @@
+ CREATE TABLE article (
+ref INTEGER PRIMARY KEY,
+prix REAL,
+libelle TEXT,
+description TEXT,
+image TEXT,
+categorie INTEGER,
+FOREIGN KEY(categorie) REFERENCES categorie(id)
+);
+
+CREATE TABLE categorie (
+  id INTEGER PRIMARY KEY,
+  nom TEXT,
+  pere INTEGER,
+  FOREIGN KEY(pere) REFERENCES categorie(id)
+  );
